7c922a81a06e52eb73a74c973f08580ffc3525e2
Ikenna Nwaiwu <ikenna4u@gmail.com>
2023-08-13 07:54:54 +0100

    Adding rate limit

--- chapter9/software-agent.sh
+++ chapter9/software-agent.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 while true
 do
-	echo "Press [CTRL+C] to stop.."
+	echo "Press [CTRL+C] to stop..."
   wget -N https://raw.githubusercontent.com/ikenna/chapter9/main/chapter9/apis/product-catalog.oas.yaml
   kusk deploy -i product-catalog.oas.yaml
 	sleep 15
